d791288cd6803af6c731feabcc2528f40123e22f
saumyatalwani <saumyaofficial007@gmail.com>
2023-12-24 00:30:31 +0530

    Fix

--- src/components/BubbleSort.jsx
+++ src/components/BubbleSort.jsx
@@ -1,114 +1,103 @@
-import { useEffect  , useRef } from "react";
-import ProgressBar from "./ProgressBar"
+import React, { useState, useEffect, useRef } from "react";
+import ProgressBar from "./ProgressBar";
 import { Link } from "react-router-dom";
+
 const BubbleSort = () => {
+  const [arr, setArr] = useState([]);
+  const copy = [89, 77, 67, 53, 34];
+  const pbRefs = useRef(Array.from({ length: 5 }, () => React.createRef()));
 
-    const handleClick = ()=>{
-        console.log(value.current.value);
-        arr.push(value.current.value)
-        setPB();
-    }
-    let arr = [];
-    let copy = [89,77,67,53,34];
-    var pb;
+  const handleClick = () => {
+    const newValue = parseInt(value.current.value, 10);
+    setArr([...arr, newValue]);
+  };
 
-    const value = useRef(0);
-    
-    function setPB() {
-        for (let k = 0; k < 5; k++) {
-            pb[k].style.width = arr[k] + "%";
-            pb[k].innerHTML = arr[k];
-        }
-    }
-    
-    function bubbleSortStep(i, j) {
-        if (j < arr.length - i - 1) {
-            if (arr[j] > arr[j + 1]) {
-                const temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-    
-            setPB();
-            console.log(arr);
-    
-            setTimeout(function () {
-                bubbleSortStep(i, j + 1);
-            }, 500);
-        } else {
-            setTimeout(function () {
-                bubbleSort(i + 1);
-            }, 1000);
-        }
-    }
-    function bubbleSort(i) {
-        if (i < arr.length - 1) {
-            // Start the inner loop with j = 0
-            bubbleSortStep(i, 0);
-        } else {
-            
-            // Sorting is complete
-            setPB();
-            console.log("sorting doen")
-        }
-        
-    }
+  function setPB() {
+    arr.forEach((value, index) => {
+      if (pbRefs.current[index].current) {
+        pbRefs.current[index].current.style.width = value + "%";
+        pbRefs.current[index].current.innerHTML = value;
+      }
+    });
+  }
 
-    const intitalize = ()=>{
-        console.log("use effect called")
-        pb = document.getElementsByClassName("progress-bar");
-        setPB();
-    }
+  function bubbleSortStep(i, j) {
+    if (j < arr.length - i - 1) {
+      if (arr[j + 1] < arr[j]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+      setPB();
+      console.log(arr);
 
-    useEffect(() => {
-        intitalize()
-    },[]);
+      setTimeout(() => {
+        bubbleSortStep(i, j + 1);
+      }, 1000);
+    } else {
+      setTimeout(() => {
+        bubbleSort(i + 1);
+      }, 1000);
+    }
+  }
+
+  function bubbleSort(i) {
+    if (i < arr.length - 1) {
+      bubbleSortStep(i, 0);
+    } else {
+      setPB();
+      console.log("sorting done");
+    }
+  }
+
+  const initialize = () => {
+    console.log("use effect called");
+    setPB();
+  };
+
+  useEffect(() => {
+    initialize();
+  }, [arr]);
+
+  const value = useRef(null);
 
   return (
     <div className="m-5">
-        <Link to="/">
+      <Link to="/">
         <h1 className="pb-4 arrow">
-        <span></span>
-        <span></span>
-        <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
         </h1>
-        </Link>
-         <h1 className="hover:underline ml-[100px] p-3"></h1>
-        <h1 className="text-3xl pb-2">Bubble Sort</h1>
-        <ProgressBar />
-        <ProgressBar />
-        <ProgressBar />
-        <ProgressBar />
-        <ProgressBar />
-        <button className="mt-3 px-5" onClick={()=>{bubbleSort(0)}}>Start Sorting</button>
-        <button className="ml-5 px-5" onClick={()=>{
-            arr=copy;
-            intitalize();
-        }}>Reset</button>
-        <form onSubmit={(e)=>{e.preventDefault()}}>
-            <input ref={value} type="text" placeholder="Enter Value" className="mt-3 p-2 rounded-xl"></input>
-            <button type="submit" className="ml-5 px-5 p-2 rounded-xl text-white bg-black bg-opacity-60" onClick={handleClick}>Submit</button>
-        </form>
-        {/* <form onSubmit={(e)=>{e.preventDefault()}}>
-            <input ref={value} type="text" placeholder="Enter Value" className="mt-3 p-2 rounded-xl"></input>
-            <button type="submit" className="ml-5 px-5 p-2 rounded-xl text-white bg-black bg-opacity-60" onClick={handleClick}>Submit</button>
-        </form>
-        <form onSubmit={(e)=>{e.preventDefault()}}>
-            <input ref={value} type="text" placeholder="Enter Value" className="mt-3 p-2 rounded-xl"></input>
-            <button type="submit" className="ml-5 px-5 p-2 rounded-xl text-white bg-black bg-opacity-60" onClick={handleClick}>Submit</button>
-        </form>
-        <form onSubmit={(e)=>{e.preventDefault()}}>
-            <input ref={value} type="text" placeholder="Enter Value" className="mt-3 p-2 rounded-xl"></input>
-            <button type="submit" className="ml-5 px-5 p-2 rounded-xl text-white bg-black bg-opacity-60" onClick={handleClick}>Submit</button>
-        </form>
-        <form onSubmit={(e)=>{e.preventDefault()}}>
-            <input ref={value} type="text" placeholder="Enter Value" className="mt-3 p-2 rounded-xl"></input>
-            <button type="submit" className="ml-5 px-5 p-2 rounded-xl text-white bg-black bg-opacity-60" onClick={handleClick}>Submit</button>
-        </form> */}
-        
-        
+      </Link>
+      <h1 className="hover:underline ml-[100px] p-3"></h1>
+      <h1 className="text-3xl pb-2">Bubble Sort</h1>
+      {Array.from({ length: 5 }, (_, index) => (
+        <ProgressBar key={index} ref={pbRefs.current[index]} />
+      ))}
+      <button className="mt-3 px-5" onClick={() => bubbleSort(0)}>
+        Start Sorting
+      </button>
+      <button className="ml-5 px-5" onClick={() => setArr(copy)}>
+        Reset
+      </button>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          ref={value}
+          type="text"
+          placeholder="Enter Value"
+          className="mt-3 p-2 rounded-xl"
+        />
+        <button
+          type="submit"
+          className="ml-5 px-5 p-2 rounded-xl text-white bg-black bg-opacity-60"
+          onClick={handleClick}
+        >
+          Submit
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default BubbleSort
+export default BubbleSort;
--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,10 +1,28 @@
-const ProgressBar = () => {
+import React, { forwardRef } from "react";
+
+const ProgressBar = forwardRef((props, ref) => {
   
   return (
     <div className="progress mb-3 mt-2" role="progressbar" style={{height : "30px"}}>
-        <div className="progress-bar bg-dark" ></div>
+        <div className="progress-bar bg-dark" ref={ref}></div>
     </div>
   )
-}
+
+});
 
 export default ProgressBar
+
+/*
+// ProgressBar.jsx
+
+
+
+
+
+const ProgressBar = forwardRef((props, ref) => {
+  return <div ref={ref} className="progress-bar"></div>;
+});
+
+export default ProgressBar;
+
+*/
